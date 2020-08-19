cd32d51fcc959c3ded13d9743868fec4dd344a3d
CodingAir <erik.zimmermann2000@yahoo.de>
2020-08-19 17:37:22 +0200

    Updates version to v4.2.10

--- src/de/codingair/warpsystem/spigot/base/utils/Notifier.java
+++ src/de/codingair/warpsystem/spigot/base/utils/Notifier.java
@@ -1,6 +1,5 @@
 package de.codingair.warpsystem.spigot.base.utils;
 
-import com.earth2me.essentials.Warps;
 import de.codingair.codingapi.files.ConfigFile;
 import de.codingair.warpsystem.spigot.base.WarpSystem;
 import de.codingair.warpsystem.spigot.base.language.Lang;
@@ -22,7 +21,7 @@ public class Notifier {
                 String v = WarpSystem.getInstance().getUpdateNotifier().getVersion();
                 if(!v.startsWith("v")) v = "v" + v;
 
-                TextComponent tc0 = new TextComponent(Lang.getPrefix() + "§7A new update is available §8[§b" + v + "§8 - §b" + WarpSystem.getInstance().getUpdateNotifier().getUpdateInfo() + "§8]§7. Download it §7»");
+                TextComponent tc0 = new TextComponent(Lang.getPrefix() + "§7A new update is available §8[§b" + WarpSystem.getInstance().getUpdateNotifier().getUpdateInfo() + "§8]§7. Download it §7»");
                 TextComponent click = new TextComponent("§chere");
                 TextComponent tc1 = new TextComponent("§7«!");
 
