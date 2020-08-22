ddf976b8bf7038a0e3dcf0b3ac156817aec23ef3
CodingAir <erik.zimmermann2000@yahoo.de>
2020-08-22 10:05:54 +0200

    [WS-460] Adds BETA function to test new features

---
+++ src/de/codingair/warpsystem/spigot/features/beta/functions/Beta.java
@@ -0,0 +1,7 @@
+package de.codingair.warpsystem.spigot.features.beta.functions;
+
+import de.codingair.warpsystem.utils.Manager;
+
+public interface Beta extends Manager {
+    String getFinalConfigTag();
+}
