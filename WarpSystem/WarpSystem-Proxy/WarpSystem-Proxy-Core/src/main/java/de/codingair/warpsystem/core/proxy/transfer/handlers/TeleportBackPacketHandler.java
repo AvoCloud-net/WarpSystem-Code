b79ebbbcfe4b1c4dad1e806189a78f3b55a3dfc6
CodingAir <erik.zimmermann2000@yahoo.de>
2021-01-20 17:39:31 +0100

    Add redis support for /back <player>

--- WarpSystem/WarpSystem-Proxy/WarpSystem-Proxy-Core/src/main/java/de/codingair/warpsystem/core/proxy/transfer/handlers/TeleportBackPacketHandler.java
+++ WarpSystem/WarpSystem-Proxy/WarpSystem-Proxy-Core/src/main/java/de/codingair/warpsystem/core/proxy/transfer/handlers/TeleportBackPacketHandler.java
@@ -1,10 +1,13 @@
 package de.codingair.warpsystem.core.proxy.transfer.handlers;
 
+import de.codingair.packetmanagement.exceptions.Escalation;
+import de.codingair.packetmanagement.handlers.ResponsibleMultiLayerPacketHandler;
 import de.codingair.packetmanagement.handlers.ResponsiblePacketHandler;
 import de.codingair.packetmanagement.packets.impl.BooleanPacket;
 import de.codingair.packetmanagement.utils.Direction;
 import de.codingair.packetmanagement.utils.Proxy;
 import de.codingair.warpsystem.core.proxy.Core;
+import de.codingair.warpsystem.core.proxy.redis.RedisCore;
 import de.codingair.warpsystem.core.proxy.utils.Player;
 import de.codingair.warpsystem.core.proxy.utils.Players;
 import de.codingair.warpsystem.core.transfer.packets.general.TeleportBackPacket;
@@ -13,12 +16,12 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
 
-public class TeleportBackPacketHandler implements ResponsiblePacketHandler<TeleportBackPacket, BooleanPacket> {
+public class TeleportBackPacketHandler implements ResponsibleMultiLayerPacketHandler<TeleportBackPacket, BooleanPacket> {
     @Override
     public @NotNull CompletableFuture<BooleanPacket> response(@NotNull TeleportBackPacket packet, @NotNull Proxy proxy, @Nullable Object connection, @NotNull Direction direction) {
         Player player = Players.getPlayer(packet.getName());
 
-        if(player == null) return CompletableFuture.completedFuture(new BooleanPacket(false));
+        if (player == null) throw new Escalation(this, Direction.UP, packet, err -> new BooleanPacket(false), RedisCore.TIME_OUT);
         else return Core.getPlugin().dataHandler().send(packet, player.getServer(), Direction.DOWN);
     }
 }
