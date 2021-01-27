e5eec47accb628386e245ceb95b674974e3a167c
CodingAir <erik.zimmermann2000@yahoo.de>
2021-01-27 09:55:16 +0100

    [WS-551] Fix missed GlobalWarps

--- WarpSystem/WarpSystem-Proxy/WarpSystem-Proxy-Velocity/src/main/java/de/codingair/warpsystem/velocity/features/globalwarps/GlobalWarpManager.java
+++ WarpSystem/WarpSystem-Proxy/WarpSystem-Proxy-Velocity/src/main/java/de/codingair/warpsystem/velocity/features/globalwarps/GlobalWarpManager.java
@@ -1,7 +1,7 @@
 package de.codingair.warpsystem.velocity.features.globalwarps;
 
-import de.codingair.warpsystem.core.transfer.utils.serializeable.SGlobalWarp;
 import de.codingair.warpsystem.core.proxy.features.GlobalWarpHandler;
+import de.codingair.warpsystem.core.transfer.utils.serializeable.SGlobalWarp;
 import de.codingair.warpsystem.velocity.api.files.ConfigFile;
 import de.codingair.warpsystem.velocity.api.files.VelocityConfigMask;
 import de.codingair.warpsystem.velocity.base.WarpSystem;
@@ -12,7 +12,6 @@ public class GlobalWarpManager extends GlobalWarpHandler {
     public boolean load(boolean loader) {
         ConfigFile file = WarpSystem.getInstance().getFileManager().getFile("GlobalWarps", "/");
 
-        WarpSystem.proxy().getEventManager().register(WarpSystem.getInstance(), new GlobalWarpListener());
         return super.load(loader, new VelocityConfigMask(file));
     }
 
