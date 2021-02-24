dd13b82a2ad78042ba6cfe5103ac0e76d12fd820
CodingAir <erik.zimmermann2000@yahoo.de>
2021-02-24 22:46:58 +0100

    [WS-596] Fix BungeeCord field for Velocity. Has been moved to Config.yml

--- WarpSystem/WarpSystem-Spigot/src/main/java/de/codingair/warpsystem/spigot/api/SpigotAPI.java
+++ WarpSystem/WarpSystem-Spigot/src/main/java/de/codingair/warpsystem/spigot/api/SpigotAPI.java
@@ -1,6 +1,5 @@
 package de.codingair.warpsystem.spigot.api;
 
-import de.codingair.codingapi.server.reflections.IReflection;
 import de.codingair.warpsystem.spigot.api.blocks.listeners.RuleListener;
 import de.codingair.warpsystem.spigot.api.packetreader.GlobalPacketReaderListener;
 import de.codingair.warpsystem.spigot.api.packetreader.GlobalPacketReaderManager;
@@ -10,26 +9,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpigotAPI {
     private static SpigotAPI instance;
-    private Boolean bungeeCord = null;
 
     private final GlobalPacketReaderManager globalPacketReaderManager = new GlobalPacketReaderManager();
 
     public static SpigotAPI getInstance() {
         if (instance == null) instance = new SpigotAPI();
         return instance;
-    }
-
-    public static boolean bungeeCord() {
-        if (instance.bungeeCord != null) return instance.bungeeCord;
-
-        try {
-            IReflection.FieldAccessor<Boolean> bungee = IReflection.getField(Class.forName("org.spigotmc.SpigotConfig"), "bungee");
-            instance.bungeeCord = bungee.get(null);
-        } catch (ClassNotFoundException ex) {
-            instance.bungeeCord = Bukkit.spigot().getConfig().getBoolean("settings.bungeecord");
-        }
-
-        return instance.bungeeCord;
     }
 
     public void onEnable(JavaPlugin plugin) {
