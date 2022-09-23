6ee5b37bae0465c4044d09721df61a501abe410f
Erik Zimmermann <mail@erikzimmermann.de>
2022-09-23 10:51:11 +0200

    Update EssentialsX repo

--- WarpSystem/WarpSystem-Spigot/src/main/java/de/codingair/warpsystem/spigot/base/utils/money/adapters/EssentialsCurrency.java
+++ WarpSystem/WarpSystem-Spigot/src/main/java/de/codingair/warpsystem/spigot/base/utils/money/adapters/EssentialsCurrency.java
@@ -3,7 +3,9 @@ package de.codingair.warpsystem.spigot.base.utils.money.adapters;
 import com.earth2me.essentials.api.Economy;
 import com.earth2me.essentials.api.NoLoanPermittedException;
 import com.earth2me.essentials.api.UserDoesNotExistException;
+import de.codingair.warpsystem.spigot.base.WarpSystem;
 import de.codingair.warpsystem.spigot.base.utils.money.Adapter;
+import net.ess3.api.MaxMoneyException;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -16,7 +18,7 @@ public class EssentialsCurrency implements Adapter {
         if (check(player)) return 0;
 
         try {
-            return Economy.getMoneyExact(player.getName()).doubleValue();
+            return Economy.getMoneyExact(player.getUniqueId()).doubleValue();
         } catch (UserDoesNotExistException e) {
             e.printStackTrace();
             return 0;
@@ -29,9 +31,10 @@ public class EssentialsCurrency implements Adapter {
 
 
         try {
-            Economy.substract(player.getName(), new BigDecimal(amount));
+            Economy.subtract(player.getUniqueId(), new BigDecimal(amount));
         } catch (UserDoesNotExistException | NoLoanPermittedException e) {
             e.printStackTrace();
+        } catch (MaxMoneyException ignored) {
         }
     }
 
@@ -40,16 +43,17 @@ public class EssentialsCurrency implements Adapter {
         if (check(player)) return;
 
         try {
-            Economy.add(player.getName(), new BigDecimal(amount));
+            Economy.add(player.getUniqueId(), new BigDecimal(amount));
         } catch (UserDoesNotExistException | NoLoanPermittedException e) {
             e.printStackTrace();
+        } catch (MaxMoneyException e) {
+            WarpSystem.getInstance().getLogger().warning("Could not deposit " + amount + " (economy) on " + player.getName() + "'s balance.");
         }
     }
 
     private boolean check(Player player) {
         if (!Bukkit.getPluginManager().isPluginEnabled("Essentials")) return true;
-
-        if (!Economy.playerExists(player.getName())) Economy.createNPC(player.getName());
+        if (!Economy.playerExists(player.getUniqueId())) Economy.createNPC(player.getName());
         return false;
     }
 }
